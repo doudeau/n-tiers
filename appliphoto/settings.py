f31be8feaf757947df1e5188ead711a164cc7327
doudeau <85609523+doudeau@users.noreply.github.com>
2022-02-27 22:44:29 +0100

    Update settings.py

--- appliphoto/settings.py
+++ appliphoto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-or=fkwsc3b)*7ev%#27g@jgadx+jnoww-1kp8)rt6_th9@h6xj'
+SECRET_KEY = 'YoursecretKey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
